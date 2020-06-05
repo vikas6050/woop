e52903723679c93c56c1b7a0d332bf8ea52b47f3
omegalk <omegarpp@gmail.com>
2020-06-05 12:33:11 +0530

    Sucuri security wordpress plugin added

---
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,7 @@
+<?php
+// datastore=auditqueue;
+// created_on=1591340251;
+// updated_on=1591340291;
+exit(0);
+?>
+1591340251_9654:"Warning: woop_admin, 127.0.0.1; Plugin activated: Sucuri Security - Auditing, Malware Scanner and Hardening (v1.8.24; sucuri-scanner\/sucuri.php)"
---
+++ wp-content/uploads/sucuri/sucuri-integrity.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=integrity;
+// created_on=1591340291;
+// updated_on=1591340291;
+exit(0);
+?>
