f23ecec1450a3f319138b7f119af5e2e78af3aa2
DESKTOP-DTMP3DN\rhino <lakshitha@rhino-partners.com>
2021-09-08 11:43:22 +0530

    WOOP.WORLD wordpress version update to 5.8

--- wp-content/uploads/sucuri/sucuri-auditqueue.php
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -5,3 +5,5 @@
 exit(0);
 ?>
 1591340251_9654:"Warning: woop_admin, 127.0.0.1; Plugin activated: Sucuri Security - Auditing, Malware Scanner and Hardening (v1.8.24; sucuri-scanner\/sucuri.php)"
+1631081387_296:"Notice: 127.0.0.1; User authentication succeeded: woop_admin"
+1631081402_6744:"Notice: woop_admin, 127.0.0.1; Post status has been changed; details: ID: 1002,Old status: new,New status: auto-draft,Title: Auto Draft"
